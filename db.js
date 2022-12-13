2cb321a449af5ee3e75f5dd79f6c71359289972a
hendrix <leandrotully@gmail.com>
2022-12-13 13:51:42 -0300

    Back de Cadastro

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const mongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
 mongoClient.connect('mongodb://localhost:27017',
     { useUnifiedTopology: true })
@@ -7,3 +8,33 @@ mongoClient.connect('mongodb://localhost:27017',
         global.connection = connection.db("Movida");
         console.log('Connected successfully');
     }).catch(error => console.log(error))
+
+function findSerie() {
+    return connection.collection('Customers')
+        .find({})
+        .toArray();
+}
+
+function insertCustomer(customer) {
+    return connection.collection('Customers').insertOne(customer);
+}
+
+function updateCustomer(id, customer) {
+    //Converte para objectId pq id vem como string no mongo só reconhece como object
+    const objectId = new ObjectId(id);
+
+    return global.collection
+        .collection('Customers')
+        .updateOne({ _id: objectId }, { $set: customer })
+}
+
+function deleteCustomer(id) {
+    const objectId = new ObjectId(id);
+
+    return global.collection
+        .collection('Customers')
+        .deleteOne({ _id: objectId })
+}
+
+module.exports = { findSerie, insertCustomer, updateCustomer, deleteCustomer }
+
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
-console.clear();
+const router = require('express')();
+const db = require('../db.js')
 
-const express = require('express');
-const router = express.Router();
-const Customer = require('../model/customer.js');
-
-/* GET home page. */
 router.get('/', async (req, res, next) => {
 
-  Customer.findSerie().then((customers) => {
+  db.findSerie().then((customers) => {
     res.render('index', { title: 'ExpressA', customers: customers });
   }).catch(error => console.log(`Error:${error}`));
 
@@ -15,6 +11,24 @@ router.get('/', async (req, res, next) => {
 
 router.get('/new', (req, res, next) => {
   res.render("customer", { title: "Cadastrar um novo cliente" });
+});
+
+router.post("/new", (req, res, next) => {
+  const { idade } = req.body;
+
+  if (!req.body.nome)
+    return res.redirect('/new?error=Informe um nome');
+
+  if (idade && !/[0-9]+/.test(idade))
+    return res.redirect('/new?error=Idade deve ser um numero');
+
+  req.body.idade = parseInt(idade);
+
+  db.insertCustomer(req.body)
+    .then((success) => {
+      console.log(`success:${success}`);
+      res.redirect("/")
+    }).catch(error => console.log(`ERROR:${error}`));
 });
 
 module.exports = router;
